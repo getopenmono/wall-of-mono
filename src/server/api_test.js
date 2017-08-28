99c0b47b4c5e7a5882304cb07ffda7bee6e3ff61
Jens Peter Secher <jp@openmono.com>
2017-08-28 17:23:24 +0200

    Initial values after reset.

---
+++ src/server/api_test.js
@@ -0,0 +1,16 @@
+/* eslint-env mocha */
+'use strict'
+
+// const expect = require('chai').expect
+// const request = require('supertest')
+// const constants = require('server/constants')
+
+describe('wall of mono API', () => {
+  // const app = require('server')
+  describe('/get', () => {
+    it('should give a board with all letters initially')
+  })
+  describe('/set', () => {
+    it('should accept a JSON structure with coordinates, color & letter')
+  })
+})
