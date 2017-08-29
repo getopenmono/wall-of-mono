d25a12b32dd60117b02c6433b9aa2ac018ad8338
Jens Peter Secher <jp@openmono.com>
2017-08-29 18:09:09 +0200

    /set endpoint.

--- src/server/api_test.js
+++ src/server/api_test.js
@@ -33,18 +33,49 @@ describe('wall of mono API', () => {
     })
   })
   describe('/set', () => {
+    const service = request(app)
     it('should accept a form with coordinates, colors & letter', done => {
-      request(app)
+      service
       .post('/set')
       .field('x', 1)
       .field('y', 2)
       .field('foreground', 'DEAD12')
-      .field('background', 'DEAD12')
+      .field('background', '01BEEF')
       .field('letter', 'Ø')
       .expect(201)
       .expect('Content-Type', /text/)
       .expect(/Success/)
-      .end(done)
+      .then(() => {
+        service
+        .get('/get')
+        .expect(200)
+        .expect(res => {
+          let base = 0
+          for (let i = 0; i < 32; ++i) {
+            let mono = res.text.substr(base, 16)
+            // There is no just no way to avoid stupid UTF-16.
+            if (mono[15] === '=') {
+              mono = res.text.substr(base, 15)
+              base += 1
+            }
+            base += 15
+            const x = i % 8
+            const y = Math.floor(i / 8)
+            expect(mono[0]).to.equal(String.fromCharCode(48 + x))
+            expect(mono[1]).to.equal(String.fromCharCode(48 + y))
+            if (x === 1 && y === 2) {
+              expect(mono.substr(2, 6)).to.equal('DEAD12')
+              expect(mono.substr(8, 6)).to.equal('01BEEF')
+              expect(mono.substr(14, 1)).to.equal('Ø')
+            } else {
+              expect(mono.substr(2, 6)).to.equal(constants.initialWall.foregroundColors[i])
+              expect(mono.substr(8, 6)).to.equal(constants.initialWall.backgroundColors[i])
+              expect(mono.substr(14, 1)).to.equal(constants.initialWall.letters[i])
+            }
+          }
+        })
+        .end(done)
+      })
     })
   })
 })
--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@
  */
 const config = require('server/config')
 const constants = require('server/constants')
+const _ = require('lodash')
 
 /*
  * Web server.
@@ -15,8 +16,8 @@ const app = express()
 /*
  * Securing headers.
  */
-// const helmet = require('helmet')
-// app.use(helmet())
+const helmet = require('helmet')
+app.use(helmet())
 
 /*
  * Form passing.
@@ -63,12 +64,13 @@ function createInitialWall () {
   for (let i = 0; i < 32; ++i) {
     const x = i % 8
     const y = Math.floor(i / 8)
-    let mono =
-      String.fromCharCode(48 + x) +
-      String.fromCharCode(48 + y) +
-      constants.initialWall.foregroundColors[i] +
-      constants.initialWall.backgroundColors[i] +
-      constants.initialWall.letters[i]
+    let mono = generateOneMono({
+      x,
+      y,
+      foreground: constants.initialWall.foregroundColors[i],
+      background: constants.initialWall.backgroundColors[i],
+      letter: constants.initialWall.letters[i]
+    })
     if (mono.length !== 15) {
       throw new Error(`Initial wall setup wrong for Mono ${i}: ${mono}`)
     }
@@ -93,11 +95,64 @@ app.post('/set', (req, res, next) => {
       throw err
     }
     console.log(fields)
+    const x = parseInt(fields.x, 10)
+    const y = parseInt(fields.y, 10)
+    const foreground = parseInt(fields.foreground, 16)
+    const background = parseInt(fields.background, 16)
+    const letter = fields.letter
+    let errors = []
+    if (_.isNaN(x) || x < 0 || x > 7) {
+      errors.push(`Wrong X coordinate, expected number 0-7 but got ${fields.x}`)
+    }
+    if (_.isNaN(y) || y < 0 || y > 3) {
+      errors.push(`Wrong Y coordinate, expected number 0-7 but got ${fields.y}`)
+    }
+    if (_.isNaN(foreground) || foreground < 0 || foreground > 0xFFFFFF) {
+      errors.push(`Wrong foreground color, expected RGB hex number but got ${fields.foreground}`)
+    }
+    if (_.isNaN(background) || background < 0 || background > 0xFFFFFF) {
+      errors.push(`Wrong background color, expected RGB hex number but got ${fields.background}`)
+    }
+    if ((letter < 'A' || letter > 'Z') && letter !== 'Æ' && letter !== 'Ø' && letter !== 'Å' && letter !== ' ' && letter !== '.' && letter !== '-') {
+      errors.push(`Wrong letter, expected A-ZÆØÅ.- or blank but got ${fields.letter}`)
+    }
+    if (errors.length > 0) {
+      res.status(400)
+      res.type('text/plain')
+      res.send(_.join(errors, '\n'))
+      return
+    }
+    updateMono({
+      x,
+      y,
+      foreground: toPaddedHexString(foreground, 6),
+      background: toPaddedHexString(background, 6),
+      letter
+    })
     res.status(201)
     res.type('text/plain')
     res.send('Success!')
   })
 })
+
+function updateMono (values) {
+  console.log(values)
+  const index = (values.y * 8 + values.x) * 16
+  wall = wall.substring(0, index) + generateOneMono(values) + wall.substring(index + 16)
+}
+
+function generateOneMono (values) {
+  return String.fromCharCode(48 + values.x) +
+      String.fromCharCode(48 + values.y) +
+      values.foreground +
+      values.background +
+      values.letter
+}
+
+function toPaddedHexString (num, len) {
+  const str = num.toString(16).toUpperCase()
+  return '0'.repeat(len - str.length) + str
+}
 
 app.get('/get', (req, res, next) => {
   res.type('text/plain')
